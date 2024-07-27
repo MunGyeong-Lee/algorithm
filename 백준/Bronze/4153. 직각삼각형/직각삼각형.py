31c7a47e91ec9c48afce77b9e8e2ec92230626d8
jane lee <moon943380@gmail.com>
2024-07-27 17:07:07 +0900

    [Bronze III] Title: 직각삼각형, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,14 @@
+
+while True :
+    num_list = list(map(int,input().split()))
+
+
+    if num_list == [0, 0, 0]:
+        break
+    else:
+        num_list.sort()
+        if num_list[0]**2+num_list[1]**2 == num_list[2]**2:
+            print('right')
+        else :
+            print('wrong')
+
