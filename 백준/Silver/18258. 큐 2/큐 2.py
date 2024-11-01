8d28637bcd19390cd6232f0a01ec99896eb8b027
jane lee <moon943380@gmail.com>
2024-11-02 01:29:47 +0900

    [Silver IV] Title: 큐 2, Time: 1780 ms, Memory: 175092 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,39 +1,36 @@
+
 import sys
 from collections import deque
-
-N = int(sys.stdin.readline().strip())
+N = int(sys.stdin.readline())
 que = deque()
-
 for _ in range(N):
     order = sys.stdin.readline().strip()
-    
-    if "push" in order:
-        _, b = order.split()
+    if "push" in order :
+        a, b = order.split()
         que.append(b)
-        
-    elif order == "pop":
+    if order == "pop":
         if len(que) > 0:
             print(que.popleft())
         else:
             print(-1)
-            
-    elif order == "size":
+    if order == "size":
         print(len(que))
-        
-    elif order == "empty":
+
+    if order == "empty":
         if len(que) > 0:
             print(0)
         else:
             print(1)
-            
-    elif order == "front":
+    if order == "front":
         if len(que) > 0:
             print(que[0])
         else:
             print(-1)
-            
-    elif order == "back":
+
+    if order == "back":
         if len(que) > 0:
             print(que[-1])
         else:
             print(-1)
+
+
