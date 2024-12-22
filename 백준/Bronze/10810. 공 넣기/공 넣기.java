59ac2621758d80baa69e31756d7f828455b422f3
jane lee <moon943380@gmail.com>
2024-12-22 13:14:14 +0900

    [Bronze III] Title: 공 넣기, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,27 +8,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] NandM = br.readLine().split(" ");
-        int N = Integer.parseInt(NandM[0]);
-        int M = Integer.parseInt(NandM[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
         // 1~N까지 바구니 배열
         int[] basket = new int[N];
-        //
-        for (int a = 0; a < M; a++){
-            String[] inputArray = br.readLine().split(" ");
 
-            int j = Integer.parseInt(inputArray[1])-1;
-            int k = Integer.parseInt(inputArray[2]);
+        for(int i = 0; i < M;i++){
+            st = new StringTokenizer(br.readLine());
+            int iIndex = Integer.parseInt(st.nextToken())-1;
+            int jIndex = Integer.parseInt(st.nextToken())-1;
+            int ball = Integer.parseInt(st.nextToken());
 
-            for (int i = Integer.parseInt(inputArray[0]) - 1; i<=j; i++){
-                basket[i] = k;
+            for(int j = iIndex; j<=jIndex;j++){
+                basket[j] = ball;
             }
         }
 
-        for(int i = 0; i<basket.length; i++){
-            if(i > 0)bw.write(" ");
-            bw.write(Integer.toString(basket[i]));
+        // 바구니 속 공 출력
+        for(int i = 0; i < N; i++){
+            if(i!=0){
+                bw.write(" ");
+            }
+            bw.write(String.valueOf(basket[i]));
         }
 
         bw.flush();
