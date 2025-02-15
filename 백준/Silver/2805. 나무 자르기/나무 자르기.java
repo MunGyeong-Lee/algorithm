9f23550c3721540deb72d2c14178d53dbc4f1272
jane lee <moon943380@gmail.com>
2025-02-16 00:06:10 +0900

    [Silver II] Title: 나무 자르기, Time: 652 ms, Memory: 163580 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -2,51 +2,65 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int M;
 
+    static long M ;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        String[] inputA = br.readLine().split(" ");
+        String[] inputArray = br.readLine().split(" ");
+        long N = Integer.parseInt(inputA[0]);
+        M = Integer.parseInt(inputA[1]);
 
         ArrayList<Integer> trees = new ArrayList<>();
-        st = new StringTokenizer(br.readLine());
-
-        int maxTree = 0;
-        for (int i = 0; i < N; i++) {
-            int height = Integer.parseInt(st.nextToken());
-            trees.add(height);
-            maxTree = Math.max(maxTree, height);
+        for (String tree : inputArray) {
+            trees.add(Integer.parseInt(tree));
         }
 
-        // 이진 탐색 실행
+        long maxTree = Collections.max(trees);
+
         System.out.println(findMethod(0, maxTree, trees));
+
     }
 
-    private static int findMethod(int start, int end, ArrayList<Integer> trees) {
-        int nextStart = start;
-        int nextEnd = end;
+    private static long findMethod(long start, long end, ArrayList<Integer> trees) {
+        long nextStart = start;
+        long nextEnd = end;
+        long bestHeight = 0;
+//        System.out.println(M);
+//        System.out.println(trees);
+
+
+//
+//        System.out.println(nextStart);
+//        System.out.println(nextEnd);
+
 
         while (nextStart <= nextEnd) {
-            int middle = (nextStart + nextEnd) / 2;
-            long result = 0; // 나무의 합을 저장할 변수 (long 사용 필수!)
+            long middle = (nextStart + nextEnd) / 2;
+            long result = 0;
 
-            // 나무를 잘라서 총 몇 미터를 얻을 수 있는지 계산
-            for (int tree : trees) {
+            for (long tree : trees) {
+//                System.out.println(i);
                 if (tree > middle) {
+//                    System.out.println(heights.get(i));
                     result += (tree - middle);
+//                    System.out.println(result);
                 }
             }
+//            System.out.println(result);
 
-            if (result >= M) {  // 필요한 만큼 나무를 얻었으면 더 높은 절단기 가능
-                nextStart = middle + 1;
-            } else {  // 필요한 나무보다 적으면 절단기 높이를 낮춰야 함
+
+            if (result >= M) {
+                bestHeight = middle;
+                nextStart = middle +1;
+            } else {
                 nextEnd = middle - 1;
             }
-        }
 
-        return nextEnd;  // 최적의 절단기 높이 반환
+        }
+        return bestHeight;
+
     }
+
 }
