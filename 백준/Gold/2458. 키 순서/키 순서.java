3ae3b22c855e4cffb3ceea7ac54ee79196cfc0a2
jane lee <moon943380@gmail.com>
2025-02-26 15:57:23 +0900

    [Gold IV] Title: 키 순서, Time: 584 ms, Memory: 37464 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -1,98 +1,59 @@
-
 import java.util.*;
 import java.io.*;
 
 public class Main{
-    static int N, M ;
-    static List<List<Integer>> graph;
-    static List<List<Integer>> reverseGraph;
-    static boolean[] visited;
     
+    static int N, M ;
+    static final int INF = 1000000;
+    static int[][] dist;
     public static void main(String[]args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
-        N = Integer.parseInt(st.nextToken());   // 학생들의 수
-        M = Integer.parseInt(st.nextToken());   // 키 비교 횟수
-        graph = new ArrayList<>(N);              // 간선 정보 저장
-        reverseGraph = new ArrayList<>();       // 역 간선 정보 저장
-
+        N = Integer.parseInt(st.nextToken()); 
+        M = Integer.parseInt(st.nextToken());
+        dist = new int[N][N];
         
-        for(int i = 0 ; i < N ; i++){
-            graph.add(new ArrayList<>());
-            reverseGraph.add(new ArrayList<>());
+        // dist 초기화
+        for(int i =0 ; i < N; i++){
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
         }
         
-        
-        for(int i = 0 ; i < M ; i ++){
+        // 키 비교 정보 저장
+        for(int i = 0 ; i < M ; i++){
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
             
-            graph.get(a-1).add(b-1);
-            reverseGraph.get(b-1).add(a-1);
+            int a = Integer.parseInt(st.nextToken())-1;
+            int b = Integer.parseInt(st.nextToken())-1;
             
+            dist[a][b] = 1;
         }
         
-        
-        
-        // System.out.println(graph.toString());
-        // System.out.println(reverseGraph.toString());
-        
-        List<Integer> result = new ArrayList<>(N); // 학생별로 나보다 큰 학생 + 작은 학생 합을 저장
-        for(int i = 0 ; i < N ; i++){
-            result.add(0);
+        // 플로이드-와샬 알고리즘
+        for(int k =0; k< N ; k++){
+            for(int i =0 ; i < N ; i++){
+                for(int j = 0; j < N; j++){
+                    
+                    if(dist[i][k] == 1 && dist[k][j] == 1){
+                        dist[i][j] = 1; //  i-> K -> j 경로가 있으면 i->j로 확정정
+                    }
+                }
+            }
         }
         
-        for(int i = 0 ; i < N ; i++){
-            
-            boolean[] visited = new boolean[N];
-            
-            int biggerValue = biggerDFS(i, visited);
-            int smallerValue = smallerDFS(i, visited);
-            
-            result.set(i, biggerValue+smallerValue);
-            
-        }
-        
+        // 각 학생별 자신보다 작은 학생수 + 큰 학생수
         int resultCnt = 0;
-        for(int i : result){
-            if(i == N-1){
-                resultCnt ++;
+        for(int i = 0 ; i < N ; i++){
+            int count = 0; 
+            for(int j = 0 ; j < N ; j++){
+                if(dist[i][j] == 1) count++ ; // 자신보다 큰 학생 수 count
+                if(dist[j][i] == 1) count++ ; // 자신보다 작은 학생 수 count
             }
+            if(count == N-1) resultCnt++ ;
         }
+        
         System.out.println(resultCnt);
-        // System.out.println(result.toString());
         
-        
-    }
-    
-    public static int biggerDFS(int start, boolean[] visited ){
-        
-        visited[start] = true;
-        int biggerCount = 0;
-        
-        for(int student : graph.get(start)){
-            if(!visited[student]){
-
-                biggerCount += 1+ biggerDFS(student, visited);
-            }
-        }
-        
-        return biggerCount;
-    } 
-    
-    public static int smallerDFS(int start, boolean[] visited){
-        
-        visited[start] = true;
-        int smallerCount = 0;
-        
-        for(int pre: reverseGraph.get(start)){
-            if(!visited[pre]){
-
-                smallerCount += 1 + smallerDFS(pre, visited);
-            }
-        }
-        return smallerCount;
     }
 }
