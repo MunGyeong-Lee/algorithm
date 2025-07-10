af586865c541aa126f962b36ac480570ef2e4def
jane lee <moon943380@gmail.com>
2025-07-10 10:41:23 +0900

    [level 1] Title: 최소직사각형, Time: 13.03 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/README.md
+++ 프로그래머스/1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491#) 
 
 ### 성능 요약
 
-메모리: 96.7 MB, 시간: 16.69 ms
+메모리: 83 MB, 시간: 13.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 10일 10:39:11
+2025년 07월 10일 10:41:21
 
 ### 문제 설명
 
--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -5,16 +5,9 @@ class Solution {
         int leftMax = 0;
         int rightMax = 0;
         for(int i =0 ; i < sizes.length ;i++){
-            
-            // 큰 수는 index 0 으로 옯기기
-            if(sizes[i][0] < sizes[i][1]){
-                int temp = sizes[i][0];
-                sizes[i][0] = sizes[i][1];
-                sizes[i][1] = temp;
-            }
-            
-            leftMax = Math.max(leftMax, sizes[i][0]);
-            rightMax = Math.max(rightMax, sizes[i][1]);
+        
+            leftMax = Math.max(leftMax, Math.max(sizes[i][0],sizes[i][1]));
+            rightMax = Math.max(rightMax, Math.min(sizes[i][0],sizes[i][1]));
         }
         System.out.println(leftMax + " " + rightMax);
         return leftMax* rightMax;
